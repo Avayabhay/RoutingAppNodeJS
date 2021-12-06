057e2d0c8f5821519142c1bc82fd31994b5e7525
Abhay Sharma <avay.abhay@gmail.com>
2021-12-06 23:05:13 +0530

    post method, adding data to databases and accessing specific data

--- router.js
+++ router.js
@@ -2,8 +2,26 @@ const express = require("express");
 const router = express.Router();
 const accounts = require("./accounts.js");
 
+//GET request
 router.get("/accounts", (req, res) => {
   res.json({ userData: accounts });
+});
+
+//POST request
+router.post("/accounts", (req, res) => {
+  var dataToBeIncludeded = req.body;
+  accounts.push(dataToBeIncludeded);
+  res.json({ userData: accounts });
+});
+
+router.get("/accounts/:id", (req, res) => {
+  const accID = Number(req.params.id);
+  const accInfo = accounts.find((item) => item.id === accID);
+  if (accInfo) {
+    res.send({ userData: accInfo });
+  } else {
+    res.status(500).send("No such ID present");
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const router = require("./router");
+const bodyParser = require("body-parser");
+const accounts = require("./accounts");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //homeRoute
 app.get("/", (req, res) => {
   res.send("Routing App has started");
 });
 
+//creating api
 app.use("/api", router);
 
 app.listen(PORT, () => {
