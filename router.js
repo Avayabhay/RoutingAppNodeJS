d27c8d0e85a3e18a24a3bac67f97c5abec5fd3ea
Abhay Sharma <avay.abhay@gmail.com>
2021-12-06 23:36:11 +0530

    PUT request

--- router.js
+++ router.js
@@ -7,13 +7,14 @@ router.get("/accounts", (req, res) => {
   res.json({ userData: accounts });
 });
 
-//POST request
+//POST request - for adding data
 router.post("/accounts", (req, res) => {
   var dataToBeIncludeded = req.body;
   accounts.push(dataToBeIncludeded);
   res.json({ userData: accounts });
 });
 
+//GET request for filtering and getting the specific data
 router.get("/accounts/:id", (req, res) => {
   const accID = Number(req.params.id);
   const accInfo = accounts.find((item) => item.id === accID);
@@ -21,6 +22,20 @@ router.get("/accounts/:id", (req, res) => {
     res.send({ userData: accInfo });
   } else {
     res.status(500).send("No such ID present");
+  }
+});
+
+//PUT request - for changing data
+router.put("/accounts/:id", (req, res) => {
+  var accID = Number(req.params.id);
+  var accInfo = accounts.find((item) => item.id === accID);
+  //   res.send({ userData: accInfo });
+  if (accInfo) {
+    var index = accounts.indexOf(accInfo);
+    accounts[index] = { ...accInfo, ...req.body };
+    res.send({ userData: accounts });
+  } else {
+    res.status(500).end("No such user exists! Hence data not Altered!");
   }
 });
 
